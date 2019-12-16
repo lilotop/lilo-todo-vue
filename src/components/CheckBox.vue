f98aecb073d0ef126cb5634af6f6ff95a6aa236f
Lilo <liloelia@gmail.com>
2019-12-16 09:08:44 +0200

    checkbox converted to svg

--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -1,6 +1,11 @@
 <template>
     <span class="check-box" @click="$emit('input',!value)" :tabindex="isReadOnly ? '': 0" @keypress.space="$emit('input',!value)">
-        <img src="../assets/check-green.png" alt="v" v-if="value || checked">
+        <span v-if="value || checked">
+            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 40 40" width="14" height="14">
+                <!--created with vectr.com (it's their built-in check-mark shape-->
+                <path d="M21.15 31.19L21.15 31.19L13.74 40L0 23.69L7.42 14.88L13.73 22.39L32.58 0L40 8.81L21.15 31.19Z"></path>
+            </svg>
+        </span>
     </span>
 </template>
 
@@ -23,9 +28,15 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    @import "../main";
+
     .check-box:focus {
         outline: black auto 2px;
+    }
+
+    svg {
+        fill: $primary;
     }
 
 </style>
