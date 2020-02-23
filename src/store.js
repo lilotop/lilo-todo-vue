67e84f7dc7fc2c0551d26b531dd479feb7b860f7
Lilo <liloelia@gmail.com>
2020-02-24 01:26:56 +0200

    - added the 'create project' feature

--- src/store.js
+++ src/store.js
@@ -34,6 +34,10 @@ let store = Vue.observable({
         await services.addTodo(todo);
         await this.loadFromServer(true);
     },
+    async addProject(project) {
+        await services.addProject(project);
+        await this.loadFromServer(true);
+    },
     async loadFromServer(forceReload) {
         if(!loaded || forceReload) {
             try {
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="projects">
+        <div class="projects__controls">
+            <button class="projects__controls__create-button btn" @click="newProject">Create New Project</button>
+        </div>
+
         <List :columns="columns" :items="projects" @selected="projectSelected"/>
         <BlockUI :block="blockUI" :text="blockMessage"></BlockUI>
+        <ModalBox v-if="modalOpen" title="Create Project" ok-button-text="Create" @ok="createProject" @cancel="closeModal">
+            <input type="text" id="new-project-name" v-model="projectForModal">
+        </ModalBox>
+        <FormValidation ref="validation" :rules="[{type:'required', id:'new-project-name', name: 'Project name'}]"></FormValidation>
+        <Toast ref="toast"></Toast>
     </div>
 </template>
 
@@ -9,10 +18,14 @@
     import store from '../store';
     import List from '../components/List';
     import BlockUI from "../components/BlockUI";
+    import ModalBox from "../components/ModalBox";
+    import FormValidation from "../components/FormValidation";
+    import { get } from "lodash";
+    import Toast from "../components/Toast";
 
     export default {
         name: 'projects',
-        components: { BlockUI, List },
+        components: { Toast, FormValidation, ModalBox, BlockUI, List },
         async mounted() {
             this.blockMessage = 'Loading projects from server...';
             this.blockUI = true;
@@ -20,9 +33,44 @@
             this.blockUI = false;
         },
         methods: {
-          projectSelected(project) {
-              this.$router.push({ name: 'todos', query: {project: project._id }});
-          }
+            projectSelected(project) {
+                this.$router.push({ name: 'todos', query: { project: project._id } });
+            },
+            newProject() {
+                this.projectForModal = '';
+                this.modalOpen = true;
+            },
+            closeModal() {
+                this.projectForModal = '';
+                this.modalOpen = false;
+            },
+            async createProject() {
+                if (!this.$refs.validation.validate()) {
+                    // has validation errors, abort
+                    return;
+                }
+
+                try {
+                    this.blockMessage = 'Creating Project...';
+                    this.blockUI = true;
+                    await store.addProject({name: this.projectForModal});
+                    this.modalOpen = false;
+                    this.projectForModal = {};
+                    this.$refs.toast.popSuccess('Project Created');
+                } catch (e) {
+                    console.log(e);
+                    let error = get(e, 'response.data.error', 'Unknown error');
+                    let errorCategory = get(e, 'response.data.errorCategory');
+                    if (errorCategory === 'duplicate') {
+                        error = 'A project with that name already exists';
+                    }
+                    this.$refs.toast.popError(error);
+                } finally {
+                    this.blockUI = false;
+                }
+
+
+            }
         },
         data() {
             return {
@@ -37,7 +85,9 @@
                         header: 'Description',
                         field: 'description'
                     }
-                ]
+                ],
+                projectForModal: '',
+                modalOpen: false
             }
         },
         computed: {
@@ -48,8 +98,15 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
     .projects {
         margin-top: 39px;
+
+        .projects__controls {
+            margin-bottom: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
     }
 </style>
