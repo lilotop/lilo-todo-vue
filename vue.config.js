b1789186602c7f4d8c3ba07e13f5de0012e39175
Lilo <liloelia@gmail.com>
2020-02-28 08:41:51 +0200

    - removed webpack extra build options as they were bloating the production build size :-(

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    configureWebpack: {
-        devtool: 'eval-source-map',
-        mode: "development"
-    },
     publicPath: process.env.NODE_ENV === 'production'
         ? '/lilo-todo-vue/'
         : '/'
