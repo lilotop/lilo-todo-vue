facac94e9d437ea7046fb686e632d66e528b6187
Lilo <liloelia@gmail.com>
2020-02-28 08:30:21 +0200

    - added sentry for monitoring errors

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueCookies from 'vue-cookies';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
+Sentry.init({
+    dsn: 'https://fd7f6f6e4edb4873b504d5e702731969@sentry.io/3111934',
+    integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
+});
 
 Vue.use(VueCookies);
 
